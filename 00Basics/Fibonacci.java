0cb002caebe6b3d3a7ffd32bbe7500e7fbba9d66
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-01-12 23:33:03 -0600

    new module-Basics and fibonacci

---
+++ 00Basics/Fibonacci.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+
+	public static void main(String[] args) {
+		
+		
+		
+
+		System.out.print(fibo(6));
+		
+	}
+
+	public static int fibo(int n){
+		
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		if(n == 2) return 1;
+
+		int first =1;
+		int second =1;
+		int num =0;
+		
+		for(int i=3; i<=n; i++){
+
+			num = first + second;
+			first = second;
+			second = num;
+		}
+		return num;
+	}
+
+}

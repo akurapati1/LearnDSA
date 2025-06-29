74750ad0ebcd00ceca4dee958913c0d962925cd8
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-06-29 11:10:24 -0500

    number of SubSequence That Satisfy The Sum Target

---
+++ 01Arrays/twoPointers/mberofSubSequenceThatSatisfyTheSumTarget.java
@@ -0,0 +1,40 @@
+package twoPointers;
+
+import java.util.Arrays;
+
+public class mberofSubSequenceThatSatisfyTheSumTarget {
+    public int numSubseq(int[] nums, int target) {
+        int mod = 1_000_000_007;
+        int count =0;
+        int[] power = new int[nums.length];
+
+        power[0] = 1;
+
+        for(int i = 1; i<nums.length; i++){
+            power[i] = (power[i-1]*2) % mod;
+        }
+        
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right){
+            if(nums[left] + nums[right] <= target){
+                count = (count + power[right-left]) % mod;
+                left++;
+            }
+            else{
+                right--;
+            }
+            
+        }
+
+        return count;
+
+
+
+        
+    }
+
+}

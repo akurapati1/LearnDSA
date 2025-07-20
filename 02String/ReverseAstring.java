9ee4f636d0e0ccafc04ce3ca5c41674a357b7df7
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-07-20 11:05:32 -0500

    ReverseStringWithoutStringBuilder

--- 02String/ReverseAstring.java
+++ 02String/ReverseAstring.java
@@ -1,8 +1,17 @@
 class ReverseAstring{
     
-    public static String reverseString(String s) {
+    public static String reverseStringwithSB(String s) {
         StringBuilder sb = new StringBuilder(s);
         return sb.reverse().toString();
+    }
+
+    public static String reverseStringwithoutSB(String s){
+        String result = "";
+        for(int i = s.length()-1; i>=0; i--){
+            result+=s.charAt(i);
+        }
+
+        return result;
     }
     
 }

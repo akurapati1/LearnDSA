905ef1fc84d8dc6eedf4d22906d71614c30d1c9b
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-07-22 02:43:23 -0500

    Sort Array

---
+++ 01Arrays/SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    
+    public static void sortArr(int[] arr){
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}

c449a4cb46d9b1f30670de1e7298f01c16e25727
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-07-22 00:06:53 -0500

    fibonacci all

--- 02String/FibonacciSeries.java
+++ 02String/FibonacciSeries.java
@@ -18,7 +18,35 @@ public class FibonacciSeries {
 
     public static void fibonacciWithoutRecursion(int n){
 
-    
+        if(n<=1){
+            System.out.println(n);
+            return;
+        } 
 
+        int first = 0;
+        int second =1;
+
+        for(int i = 2; i<n; i++){
+
+            int temp = first+second;
+
+            System.out.println(temp);
+
+            first = second;
+            second = temp;
+
+        }
+
+    }
+
+    public static int fibonacciWithDP(int n, int[] fibo){
+
+        if(n<=1) return n;
+        if(fibo[n] == 0){
+
+            fibo[n] = fibonacciWithDP(n-1, fibo)+ fibonacciWithDP(n-2, fibo);
+            
+        }
+        return fibo[n];
     }
 }

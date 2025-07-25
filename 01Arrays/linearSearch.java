f4b5a56475abf9c6d5f93ac71f737903491737c3
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-07-25 10:36:25 -0500

    linear search

---
+++ 01Arrays/linearSearch.java
@@ -0,0 +1,12 @@
+public class linearSearch {
+    
+    public static int linear(int[] arr, int k){
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == k) return i;
+        }
+
+        return -1;
+
+    }
+}
